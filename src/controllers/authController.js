d8849a7135bf0dbd1f5cb5a223b52d9fa5e300ce
Guilherme <guilhermepolicarpio@gmail.com>
2022-10-12 09:40:47 -0300

    Login implemented

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,17 +7,36 @@ export async function signUp(req,res){
 
     try{
         const passwordHash = bcrypt.hashSync(password, 10);
-
         connection.query(`INSERT INTO users (name,email, password) VALUES ($1, $2, $3)`, [name, email, passwordHash])
-
         res.status(201).send("User created with success!")
-        
-
-        
 
     }catch (error){
         console.log(error)
         res.sendStatus(422)
     }
+}
 
+export async function signIn(req,res){
+
+    const { email, password} = req.body;
+
+    try{
+        const user = (await connection.query(`SELECT * FROM users WHERE email = $1`,[email])).rows[0]
+        console.log(user)
+
+        if(user.rowCount===0 )
+            return res.status(401).send("Invalid email")
+
+    
+        const passwordValidation = bcrypt.compareSync(password, user.password)
+        
+        if(!passwordValidation)
+            return res.status(401).send("Invalid password")
+
+        res.status(201).send("Login with success!")
+
+    }catch(error){
+        console.log(error)
+        res.sendStatus(422)
+    }
 }
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,11 @@
 import { Router} from 'express'
-import { signUp } from '../controllers/authController.js';
+import { signUp,signIn } from '../controllers/authController.js';
 import signUpSchema from '../schemas/authSchema.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const authRouter = Router();
 
 authRouter.post("/signup", authMiddleware(signUpSchema),signUp)
+authRouter.get("/signin", signIn)
 
 export default authRouter
