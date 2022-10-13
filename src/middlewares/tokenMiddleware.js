0d7f32ff0c39a03b9a77129e2fb9f8c8101b58c0
Guilherme <guilhermepolicarpio@gmail.com>
2022-10-13 14:40:16 -0300

    Token validation implemented

---
+++ src/middlewares/tokenMiddleware.js
@@ -0,0 +1,27 @@
+import connection from "../database.js";
+
+const tokenValidation = async (req,res,next) =>{
+
+    const { Authorization } = req.headers;
+
+    const token = Authorization?.replace('Bearer ', '');
+
+    if(!token) 
+        return res.status(401).send("Invalid token!")
+    
+    try{
+        const session = (await connection.query(`SELECT * FROM sessions WHERE token = $1`, [token])).rows[0];
+        
+        if(!session)
+            return res.status(401).send("Session expired!")
+
+        res.locals.userId = session.userId;
+        res,locals.token = session.token;
+        next();
+
+
+    }catch (error){
+        return res.status(500).send("Error connection!")
+    }
+}
+export default tokenValidation;
